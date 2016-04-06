07238244105514d4db14c9ea4a48325ed7edf966
Matthew Stadelman <stadelmanma@gmail.com>
2016-04-06 07:48:41 -0400

    .

--- ApertureMapModelTools/UnitConversion/__SI__.py
+++ ApertureMapModelTools/UnitConversion/__SI__.py
@@ -13,7 +13,7 @@ import re
 class SI:
     r"""
     Holds basic sub class implementation as well as SI prefixes. Meant to
-    serve as an abstract class for the other conversion classes. 
+    serve as an abstract class for the other conversion classes. test78
     """
     #
     si_prefixes = {
