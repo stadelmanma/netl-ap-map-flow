0462e13181fb86eb89e90ab04c696d1d4e8d11b0
Matthew Stadelman <stadelmanma@gmail.com>
2016-08-25 16:57:08 -0400

    Update TestCore.py

--- test/unit/TestCore.py
+++ test/unit/TestCore.py
@@ -95,7 +95,7 @@ class TestCore:
         """
         logger = amt_core._get_logger('ApertureMapModelTools')
         assert logger.name == 'AMT'
-        assert len(logger.handlers) == 1
+        assert len(logger.handlers) == 0
         assert isinstance(logger.handlers[0], logging.StreamHandler)
         assert logger.handlers[0].level == 10
         assert logger.level == 30
