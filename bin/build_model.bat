84d2a1b0b87ce65d2047b7d7b2128c451a0e52bb
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-04 14:59:45 -0400

    Remove code to create symlinks in user-site directory
    This is being removed in favor of using setup.py

--- bin/build_model.bat
+++ bin/build_model.bat
@@ -34,33 +34,3 @@ if NOT EXIST "source\dist" (
 )
 make %1 MODELNAME=apm-lcl-model.exe -C source
 move source\dist\apm-lcl-model.exe .
-::
-:: handles creating a directory junction for the module if needed
-:: this allows global import ApertureMapModelTools statments
-::
-:: getting python command if one exists
-set PYTHON_EXE=python3
-where %PYTHON_EXE% >nul 2>nul
-if /I "%errorlevel%" NEQ "0" (
-    set PYTHON_EXE=python
-)
-where %PYTHON_EXE% >nul 2>nul
-if /I "%errorlevel%" NEQ "0" (
-   exit /B
-)
-:: storing path to Python user site directory
-for /f %%i in ('python -m site --user-site') do set SITE_PATH=%%i
-:: Creating site directory first if necessary
-if NOT EXIST "%SITE_PATH%" (
-    mkdir "%SITE_PATH%"
-)
-:: Symlinking the module into Python site
-if NOT EXIST "%SITE_PATH%\ApertureMapModelTools" (
-    mklink /J "%SITE_PATH%\ApertureMapModelTools" "%REPO_HOME%\ApertureMapModelTools"
-    :: Checking return code of mklink command
-    if /I "%errorlevel%" NEQ "0" (
-        echo Error code %errorlevel% when attempting to symlink ApertureMapModelTools module
-    ) else (
-        echo Succesfully symlinked ApertureMapModelTools module to Python site
-    )
-)
