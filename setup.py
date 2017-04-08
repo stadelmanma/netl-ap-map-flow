ed09fdb04ef8567929ff42a407c521135e0e8bf4
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-07 22:32:29 -0400

    Remove code to compile model from setup.py
    When I get this on conda-forge the user will download a version
    already packaged with a binary. If developing the model then you
    will simply need to run the script

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from glob import glob
 import os
-from subprocess import Popen
 import sys
 
 try:
@@ -25,16 +24,6 @@ with open(ver_path) as f:
     for line in f:
         if line.startswith('__version__'):
             exec(line, main_)
-        if line.startswith('DEFAULT_MODEL_NAME'):
-            exec(line, main_)
-
-# check for makefile target and remove args to prevent errors in setup()
-try:
-    arg_loc = sys.argv.index('--make-target')
-    del sys.argv[arg_loc]
-    target = sys.argv.pop(arg_loc)
-except ValueError:
-    target = None
 
 # call setup
 setup(
@@ -75,14 +64,3 @@ setup(
     license='GPLv3',
     keywords=['Local Cubic Law', 'Paraview', 'OpenFoam']
 )
-
-# build the model with an optional target, i.e. debug
-cmd = ['./bin/build_model', '-n', main_['DEFAULT_MODEL_NAME']]
-if target:
-    cmd.append(target)
-#
-proc = Popen(cmd)
-proc.wait()
-if proc.returncode != 0:
-    print('Failed to build local cubic law model')
-    sys.exit(1)
