ed36d906268322aa545ef2e4ca4a25be7ae9b0d2
Matthew Stadelman <stadelmanma@gmail.com>
2017-04-30 23:31:12 -0400

    Add thorough documentation for functions in unit_conversion.py

--- apmapflow/unit_conversion.py
+++ apmapflow/unit_conversion.py
@@ -1,10 +1,15 @@
 """
-Handles unit conversions utilizing the pint module.
-#
-Written By: Matthew Stadelman
-Date Written: 2017/03/05
-Last Modifed: 2017/03/05
-#
+================================================================================
+Unit Conversion
+================================================================================
+| Handles unit conversions utilizing the pint module.
+
+| Written By: Matthew Stadelman
+| Date Written: 2017/03/05
+| Last Modifed: 2017/04/30
+
+|
+
 """
 from pint import UnitRegistry as __UnitRegistry
 
@@ -17,13 +22,49 @@ def register_voxel_unit(voxel_size, unit):
     r"""
     Registers the 'voxel' unit with the unit_registry. voxel_size is the length
     of an edge of the voxel in the specified units.
+
+    Parameters
+    ----------
+    voxel_size : float
+        edge length of a voxel cube.
+    unit : string
+        units the voxel size is defined in.
+
+    Returns
+    -------
+    None
+
+    Examples
+    --------
+    >>> import apmapflow.unit_conversion as uc
+    >>> uc.register_voxel_unit(26.8, 'um')
+    >>> print(uc.unit_registry('voxel').to('m').magnitude)
     """
     unit_registry.define('voxel = {} * {} = vox'.format(voxel_size, unit))
 
 
 def convert_value(value, unit_in, unit_out='SI'):
     r"""
-    Returns a converted value.
+    Returns a converted value, in the specified output units or SI.
+
+    Parameters
+    ----------
+    value : float
+        edge length of a voxel cube.
+    unit_in : string
+        the current units of the value
+    unit_out : string, optional
+        the desired output units, if omitted they are converted to SI
+
+    Returns
+    -------
+    A floating point value converted to the desired units
+
+    Examples
+    --------
+    >>> import apmapflow.unit_conversion as uc
+    >>> print(uc.convert_value(26.8, 'um'))
+    >>> print(uc.convert_value(26.8, 'um', 'cm'))
     """
     quant = unit_registry.Quantity(value, unit_in)
     #
@@ -37,7 +78,25 @@ def convert_value(value, unit_in, unit_out='SI'):
 
 def get_conversion_factor(unit_in, unit_out='SI'):
     r"""
-    Returns a conversion factor.
+    Returns a conversion factor between the input unit and output unit or to SI
+    if no output unit is provided.
+
+    Parameters
+    ----------
+    unit_in : string
+        the desired input units
+    unit_out : string, optional
+        the desired output units, if omitted the SI value is used
+
+    Returns
+    -------
+    A floating point value that can be used to convert between the two units.
+
+    Examples
+    --------
+    >>> import apmapflow.unit_conversion as uc
+    >>> print(uc.get_conversion_factor('micron'))
+    >>> print(uc.get_conversion_factor('um', 'mm'))
     """
     if unit_out.upper() == 'SI':
         factor = unit_registry(unit_in).to_base_units().magnitude
