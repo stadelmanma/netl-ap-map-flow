a57dadf052fdffaa31cd4eeaeb4b3009ed3675c1
Matthew Stadelman <stadelmanma@gmail.com>
2017-05-01 22:24:02 -0400

    Adjust doc string to show table of contents

--- apmapflow/run_model/__init__.py
+++ apmapflow/run_model/__init__.py
@@ -8,7 +8,13 @@ Run Model
 | Date Written: 2016/06/16
 | Last Modifed: 2017/04/05
 
+.. toctree::
+    :maxdepth: 2
+
+    run_model/bulk_run.rst
+    run_model/run_model.rst
+
 """
-from .run_model import ArgInput, InputFile, estimate_req_RAM, run_model
+from .run_model import InputFile, estimate_req_RAM, run_model
 from .run_model import DEFAULT_MODEL_PATH, DEFAULT_MODEL_NAME
 from .bulk_run import BulkRun
