a8d697ed8c732cce54d6a7629de1d2c184d9146c
Matthew Stadelman <stadelmanma@gmail.com>
2017-05-04 23:04:28 -0400

    Add docstring to fracture df script

--- apmapflow/scripts/apm_fracture_df.py
+++ apmapflow/scripts/apm_fracture_df.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 r"""
-Script designed to take a TIF stack and determine the fractal dimension
-coefficent.
+Description: Reads in a binary fracture image stack and calculates the
+fractal dimension (Df) along either the X and/or Z axis.
+
+For usage information run: ``apm_fracture_df -h``
+
+Written By: Matthew stadelman
+Date Written: 2016/11/17
+Last Modfied: 2017/04/23
 """
 import argparse
 from argparse import RawDescriptionHelpFormatter as RawDesc
@@ -12,21 +18,12 @@ from scipy import stats as sp_stats
 from apmapflow import _get_logger, set_main_logger_level
 from apmapflow import FractureImageStack
 
-#
-desc_str = r"""
-Description: Reads in a tiff stack from the CT scanner and calculates the
-fractal dimension (Df) along either the X and/or Z axis.
-
-Written By: Matthew stadelman
-Date Written: 2016/11/17
-Last Modfied: 2017/04/23
-"""
 # setting up logger
 set_main_logger_level('info')
 logger = _get_logger('apmapflow.Scripts')
 
 # creating arg parser
-parser = argparse.ArgumentParser(description=desc_str, formatter_class=RawDesc)
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=RawDesc)
 
 # adding arguments
 parser.add_argument('-f', '--force', action='store_true',
@@ -265,9 +262,11 @@ def calculate_df(line_trace):
     the fracture profile and the standard deviation of the displacement of the
     profile at the ends of the window is calculated
 
-                       N-s
-    sigma_s = [(1/N-s)*SUM((Profile_height(i) - Profile_height(i+s))^2)]^1/2
-                       i=1
+    Formulation::
+
+                           N-s
+        sigma_s = [(1/N-s)*SUM((Profile_height(i) - Profile_height(i+s))^2)]^1/2
+                           i=1
 
     where sigma_s = incremental standard deviation
     N = number of data points
